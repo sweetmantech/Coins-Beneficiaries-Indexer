3ce547f8096d14a01094920c170aa64b9cfa57ff
sweetman.eth <sweetmantech@gmail.com>
2025-10-21 10:29:10 -0500

    Initial commit

---
+++ src/EventHandlers.ts
@@ -0,0 +1,20 @@
+/*
+ * Please refer to https://docs.envio.dev for a thorough guide on all Envio indexer features
+ */
+import {
+  LockableUniswapV3Initializer,
+  LockableUniswapV3Initializer_Collect,
+} from "generated";
+
+LockableUniswapV3Initializer.Collect.handler(async ({ event, context }) => {
+  const entity: LockableUniswapV3Initializer_Collect = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    pool: event.params.pool,
+    beneficiary: event.params.beneficiary,
+    fees0: event.params.fees0,
+    fees1: event.params.fees1,
+    transactionHash: event.transaction.hash,
+    blockNumber: event.block.number,
+  };
+  context.LockableUniswapV3Initializer_Collect.set(entity);
+});
