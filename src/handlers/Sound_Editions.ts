2c04c0bb0e0b41529c47b92bc8aba221eaa89ef2
ziad <tech.eng.me@gmail.com>
2026-03-31 12:36:20 +1000

    fix: sound_admins

--- src/handlers/Sound_Editions.ts
+++ src/handlers/Sound_Editions.ts
@@ -1,10 +1,14 @@
 import { decodeInitData } from "../../lib/sound_editions/decodeInitData";
+import { getLatestAdmin } from "@/lib/sound_admins/getLatestAdmin";
 import {
   SoundCreatorV2,
   type Sound_Editions,
+  type Sound_Admins,
   type SoundCreatorV2_Created_handlerArgs,
   type contractRegistrations,
 } from "generated";
+
+const ADMIN_ROLE = 1;
 
 SoundCreatorV2.Created.contractRegister(
   ({
@@ -20,17 +24,31 @@ SoundCreatorV2.Created.contractRegister(
 
 SoundCreatorV2.Created.handler(async ({ event, context }: SoundCreatorV2_Created_handlerArgs) => {
   const address = event.params.edition.toLowerCase();
+  const owner = event.params.owner.toLowerCase();
   const { name, contractURI } = decodeInitData(event.params.initData as string);
-  const entity: Sound_Editions = {
+
+  const edition: Sound_Editions = {
     id: `${address}_${event.chainId}`,
     address,
     name,
-    owner: event.params.owner.toLowerCase(),
+    owner,
     uri: contractURI,
     chain_id: event.chainId,
     created_at: event.block.timestamp,
     updated_at: event.block.timestamp,
     transaction_hash: event.transaction.hash,
   };
-  context.Sound_Editions.set(entity);
+  context.Sound_Editions.set(edition);
+
+  const adminEntity: Sound_Admins = {
+    id: `${address}_${event.chainId}_0_${owner}`,
+    collection: address,
+    token_id: BigInt(0),
+    admin: owner,
+    roles: ADMIN_ROLE,
+    chain_id: event.chainId,
+    updated_at: event.block.timestamp,
+  };
+  const latestAdmin = await getLatestAdmin(adminEntity, context);
+  context.Sound_Admins.set(latestAdmin);
 });
