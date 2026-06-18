33b0a42326d516ac9d14727c4e15bb7ed0cf8333
ziad <tech.eng.me@gmail.com>
2026-06-18 11:21:16 +1000

    Revert "feat: restore Zora comments and transfer indexing"
    This reverts commit b41550e32287fa516d6ba767399ac423ac061e87.

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -17,6 +17,4 @@ import "./handlers/Sound_Moments";
 import "./handlers/Sound_Sales";
 import "./handlers/Sound_Transfers";
 import "./handlers/Zora_Collections";
-import "./handlers/Zora_Comments";
 import "./handlers/Zora_Moments";
-import "./handlers/Zora_Transfers";
--- src/handlers/Zora_Transfers.ts
+++
@@ -1,9 +0,0 @@
-import { ZoraCreator1155, type ZoraCreator1155_TransferSingle_handlerArgs } from "generated";
-import { zeroAddress } from "viem";
-import { handleTransferSingleMint } from "@/lib/zora_protocol/handleTransferSingleMint";
-
-ZoraCreator1155.TransferSingle.handler(
-  async ({ event, context }: ZoraCreator1155_TransferSingle_handlerArgs) =>
-    handleTransferSingleMint(event, context),
-  { eventFilters: [{ from: zeroAddress }] }
-);
