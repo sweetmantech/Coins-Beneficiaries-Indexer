44eb9e0af99c3302d08f29629867fae34109b884
ziad <tech.eng.me@gmail.com>
2026-09-01 09:47:21 +1000

    Update Zora_Moments.ts

--- src/handlers/Zora_Moments.ts
+++ src/handlers/Zora_Moments.ts
@@ -32,16 +32,14 @@ ZoraCreator1155.URI.handler(async ({ event, context }: ZoraCreator1155_URI_handl
   });
 });
 
-InPublic1155.URI.handler(
-  async ({ event, context }: InPublic1155_URI_handlerArgs) => {
-    const id = `${event.srcAddress.toLowerCase()}_${event.params.id}_${event.chainId}`;
-    const existing = await context.Zora_Moments.get(id);
-    if (!existing) return;
+InPublic1155.URI.handler(async ({ event, context }: InPublic1155_URI_handlerArgs) => {
+  const id = `${event.srcAddress.toLowerCase()}_${event.params.id}_${event.chainId}`;
+  const existing = await context.Zora_Moments.get(id);
+  if (!existing) return;
 
-    context.Zora_Moments.set({
-      ...existing,
-      uri: event.params.value,
-      updated_at: event.block.timestamp,
-    });
-  }
-);
+  context.Zora_Moments.set({
+    ...existing,
+    uri: event.params.value,
+    updated_at: event.block.timestamp,
+  });
+});
